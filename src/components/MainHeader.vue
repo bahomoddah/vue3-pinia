396b406618f86198ad0d56d886f7711addd784b3
MohBahomoddah <mohammedbahomoddah@bootfi.com>
2023-01-17 20:39:41 +0300

    add font aswome icons

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,5 +1,27 @@
 <template>
-  <router-link to="/" class="btn btn-primary active m-2">Home</router-link>
-  <router-link to="/stores/" class="btn btn-primary m-2">Stores</router-link>
-  <router-link to="/login/" class="btn btn-primary m-2">Login</router-link>
+<nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <router-link to="/" class="navbar-brand">
+      <img src="/public/vite.svg" alt="" width="30" height="24" class="d-inline-block align-text-top">
+      Bootstrap
+    </router-link>
+    <div>
+    
+  
+        <div class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <i class="fa fa-heart"></i>
+            محمد أحمد
+
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">            
+            <li><router-link to="/" class="dropdown-item">Home</router-link></li>
+            <li><router-link to="/stores/" class="dropdown-item">Stores</router-link></li>
+            <li><router-link to="/login/" class="dropdown-item">Login</router-link></li>
+          </ul>
+        </div>
+      
+    </div>
+  </div>
+</nav>
 </template>
