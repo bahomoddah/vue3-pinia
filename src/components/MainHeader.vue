928bda27efbe700da7cfc5fe46896f0b542f7ae1
MohBahomoddah <mohammedbahomoddah@bootfi.com>
2023-01-17 19:13:36 +0300

    adding bootstrap and sass

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-link to="/">Home</router-link>
-  <router-link to="/stores/">Stores</router-link>
-  <router-link to="/login/">Login</router-link>
+  <router-link to="/" class="btn btn-primary active m-2">Home</router-link>
+  <router-link to="/stores/" class="btn btn-primary m-2">Stores</router-link>
+  <router-link to="/login/" class="btn btn-primary m-2">Login</router-link>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import { createApp } from 'vue'
-import './style.css'
+import './style.scss'
 import App from './App.vue'
 import store from './stores'
 import router from './router'
+
+import "bootstrap/dist/css/bootstrap.css";
 
 const vue = createApp(App);
 vue.use(router);
 vue.use(store);
 
 vue.mount('#app');
+
+import "bootstrap/dist/js/bootstrap.js";
